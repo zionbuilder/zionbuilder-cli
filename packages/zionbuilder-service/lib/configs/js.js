383ef610c9aeb87cd19ab066eaf8994dfbf656e6
Balasa Sorin Stefan <stefan@hogash.com>
2020-03-04 22:50:11 +0200

    Fixed missing imports

--- packages/zionbuilder-service/lib/configs/js.js
+++ packages/zionbuilder-service/lib/configs/js.js
@@ -2,47 +2,52 @@ const path = require('path')
 const babel = require('@babel/core')
 
 module.exports = (webpackConfig, service) => {
-    const useThreads = process.env.NODE_ENV === 'production' && !!service.options.getOption('parallel')
-    webpackConfig.resolveLoader.modules.prepend(path.join(__dirname, 'node_modules'))
+	const useThreads = process.env.NODE_ENV === 'production' && !!service.options.getOption('parallel')
+	webpackConfig.resolveLoader.modules.prepend(path.join(__dirname, 'node_modules'))
 
-    const jsRule = webpackConfig.module
-      .rule('js')
-        .test(/\.m?jsx?$/)
-        .exclude
-          .add(filepath => {
-            // always transpile js in vue files
-            if (/\.vue\.jsx?$/.test(filepath)) {
-              return false
-            }
+	const babelOptions = {
+		presets: [require.resolve('@zionbuilder/babel-preset-zionbuilder')]
+	}
 
-            // Don't transpile node_modules
-            return /node_modules/.test(filepath)
-          })
-          .end()
-        .use('cache-loader')
-          .loader(require.resolve('cache-loader'))
-          .options(service.genCacheConfig('babel-loader', {
-            '@babel/core': require('@babel/core/package.json').version,
-            '@zionbuilder/babel-preset-zionbuilder': require('../../../babel-preset-zionbuilder/package.json').version,
-            'babel-loader': require('babel-loader/package.json').version,
-            browserslist: service.pkg.browserslist
-          }, [
-            'babel.config.js',
-            '.browserslistrc'
-          ]))
-          .end()
+	const jsRule = webpackConfig.module
+	  .rule('js')
+		.test(/\.m?jsx?$/)
+		.exclude
+			.add(filepath => {
+				// always transpile js in vue files
+				if (/\.vue\.jsx?$/.test(filepath)) {
+					return false
+				}
 
-    if (useThreads) {
-      const threadLoaderConfig = jsRule
-        .use('thread-loader')
-          .loader(require.resolve('thread-loader'))
+				// Don't transpile node_modules
+				return /node_modules/.test(filepath)
+			})
+			.end()
+		.use('cache-loader')
+			.loader(require.resolve('cache-loader'))
+			.options(service.genCacheConfig('babel-loader', {
+				'@babel/core': require('@babel/core/package.json').version,
+				'@zionbuilder/babel-preset-zionbuilder': require('@zionbuilder/babel-preset-zionbuilder').version,
+				'babel-loader': require('babel-loader/package.json').version,
+				browserslist: service.pkg.browserslist
+			}, [
+				'babel.config.js',
+				'.browserslistrc'
+			]))
+			.end()
 
-      if (typeof options.parallel === 'number') {
-        threadLoaderConfig.options({ workers: options.parallel })
-      }
-    }
+	if (useThreads) {
+		const threadLoaderConfig = jsRule
+			.use('thread-loader')
+			.loader(require.resolve('thread-loader'))
 
-    jsRule
-      .use('babel-loader')
-        .loader(require.resolve('babel-loader'))
+		if (typeof options.parallel === 'number') {
+			threadLoaderConfig.options({ workers: options.parallel })
+		}
+	}
+
+	jsRule
+		.use('babel-loader')
+			.tap(() => babelOptions)
+			.loader(require.resolve('babel-loader'))
 }
--- packages/zionbuilder-service/tasks/watch.js
+++ packages/zionbuilder-service/tasks/watch.js
@@ -9,103 +9,105 @@ const { elementScriptsTask, elementStylesTask } = require('./scripts')
 const service = process.ZIONBUILDER_SERVICE
 const options = service.options
 
-function watchTask () {    
-		console.log( chalk.blue('ZionBuilder Service is watching element scripts.') )
-		
-		const elementsFolder = options.getOption('elementsFolder')
-		watch(`${elementsFolder}/**/src/element.js`, elementScriptsTask)
-		watch(`${elementsFolder}/**/src/element.scss`, elementStylesTask)
-		
-		// webpack
-		const port = service.availablePort
-		service.chainWebpack(webpackConfig => {
-			webpackConfig
-				.devtool('cheap-module-eval-source-map')
+function watchTask () {
+	console.log( chalk.blue('ZionBuilder Service is watching element scripts.') )
 
-			webpackConfig
-				.plugin('hmr')
-				.use(require('webpack/lib/HotModuleReplacementPlugin'))
+	const elementsFolder = options.getOption('elementsFolder')
+	watch(`${elementsFolder}/**/src/element.js`, elementScriptsTask)
+	watch(`${elementsFolder}/**/src/element.scss`, elementStylesTask)
 
-			// https://github.com/webpack/webpack/issues/6642
-			// https://github.com/vuejs/vue-cli/issues/3539
-			webpackConfig
-				.output
-				.globalObject(`(typeof self !== 'undefined' ? self : this)`)
+	// webpack
+	const port = service.availablePort
+	service.chainWebpack(webpackConfig => {
+		webpackConfig
+			.devtool('cheap-module-eval-source-map')
 
-			if (options.getOption('progress', true) !== false) {
-				webpackConfig
-					.plugin('progress')
-					.use(require('webpack/lib/ProgressPlugin'))
+		webpackConfig
+			.plugin('hmr')
+			.use(require('webpack/lib/HotModuleReplacementPlugin'))
+
+		// https://github.com/webpack/webpack/issues/6642
+		// https://github.com/vuejs/vue-cli/issues/3539
+		webpackConfig
+			.output
+			.globalObject(`(typeof self !== 'undefined' ? self : this)`)
+
+		if (options.getOption('progress', true) !== false) {
+			webpackConfig
+				.plugin('progress')
+				.use(require('webpack/lib/ProgressPlugin'))
+		}
+	})
+
+	const WebpackDevServer = require('webpack-dev-server')
+	const webpackConfig = merge(service.resolveWebpackConfig(), {
+		mode: 'development'
+	})
+
+	const compiler = webpack(webpackConfig)
+	const server = new WebpackDevServer(compiler, {
+		logLevel: 'silent',
+		clientLogLevel: 'silent',
+		headers: {
+			'Access-Control-Allow-Origin': '*'
+		},
+		allowedHosts: ['*'],
+		disableHostCheck: true,
+		overlay: {
+			warnings: false,
+			errors: true
+		},
+		port,
+		injectClient: true,
+		hot: true,
+		inline: true,
+		hotOnly: true,
+		writeToDisk (filePath) {
+			// Only write our own files to disk
+			return !/hot-update\.(json|js)$/.test(filePath)
+		},
+		publicPath: service.getPublicPath()
+	})
+
+	;['SIGINT', 'SIGTERM'].forEach(signal => {
+		process.on(signal, () => {
+			server.close(() => {
+			process.exit(0)
+			})
+		})
+	})
+
+	if (process.argv.stdin) {
+		process.stdin.on('end', () => {
+			server.close(() => {
+			process.exit(0)
+			})
+		})
+
+		process.stdin.resume()
+	}
+
+	return new Promise(resolve => {
+		console.log()
+		console.log(chalk.blue(`Starting server on http://localhost:${port}`));
+		console.log()
+
+		compiler.hooks.done.tap('zionbuilder-service serve', stats => {
+			if (stats.hasErrors()) {
+				return
 			}
 		})
 
-		const WebpackDevServer = require('webpack-dev-server')
-		const webpackConfig = merge(service.resolveWebpackConfig(), {
-			mode: 'development'
+		resolve({
+			server
 		})
 
-		const compiler = webpack(webpackConfig)
-		const server = new WebpackDevServer(compiler, {
-			logLevel: 'silent',
-			clientLogLevel: 'silent',
-			headers: {
-				'Access-Control-Allow-Origin': '*'
-			},
-			allowedHosts: ['*'],
-			disableHostCheck: true,
-			overlay: { 
-				warnings: false, 
-				errors: true 
-			},
-			port,
-			injectClient: true,
-			hot: true,
-			inline: true,
-			hotOnly: true,
-			writeToDisk (filePath) {
-				// Only write our own files to disk
-				return !/hot-update\.(json|js)$/.test(filePath)
-			},
-			publicPath: service.getPublicPath()
+		server.listen(port, 'localhost', err => {
+			if (err) {
+				reject(err)
+			}
 		})
-
-		;['SIGINT', 'SIGTERM'].forEach(signal => {
-			process.on(signal, () => {
-				server.close(() => {
-				process.exit(0)
-				})
-			})
-		})
-	  
-		if (args.stdin) {
-			process.stdin.on('end', () => {
-				server.close(() => {
-				process.exit(0)
-				})
-			})
-		
-			process.stdin.resume()
-		}
-
-		return new Promise(resolve => {
-			compiler.hooks.done.tap('zionbuilder-service serve', stats => {
-				if (stats.hasErrors()) {
-					return
-				}
-			})
-
-			resolve({
-				server
-			})
-
-			server.listen(port, 'localhost', err => {
-				console.log(`Starting server on http://localhost:${port}`);
-
-				if (err) {
-					reject(err)
-				}
-			})
-		})
+	})
 }
 
 exports.default = series(watchTask)
